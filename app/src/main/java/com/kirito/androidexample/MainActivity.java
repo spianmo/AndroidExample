f96ed9b809e52e56972ac0e62d7c4e9b3baa42ae
Finger <Finger@spianmo.com>
2021-03-15 18:33:10 +0800

    Initial commit

---
+++ app/src/main/java/com/kirito/androidexample/MainActivity.java
@@ -0,0 +1,82 @@
+package com.kirito.androidexample;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.eminayar.panter.PanterDialog;
+import com.google.android.material.textfield.TextInputEditText;
+import com.kirito.androidexample.widget.bling.Bling;
+import com.kirito.androidexample.widget.bling.BlingType;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Bling mBling;
+    int[] colors = new int[]{
+            Color.parseColor("#F44336"),
+            Color.parseColor("#3F51B5"),
+            Color.parseColor("#009688"),
+
+            Color.parseColor("#E91E63"),
+            Color.parseColor("#2196F3"),
+            Color.parseColor("#5AB963"),
+            Color.parseColor("#FFC107"),
+
+            Color.parseColor("#9C28B0"),
+            Color.parseColor("#03A9F4"),
+            Color.parseColor("#8BC34A"),
+            Color.parseColor("#FF9800"),
+
+            Color.parseColor("#673AB7"),
+            Color.parseColor("#00BCD4"),
+            Color.parseColor("#CDDC39"),
+            Color.parseColor("#FF5722"),
+    };
+    private String TAG = MainActivity.class.getSimpleName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mBling = new Bling.Builder((ViewGroup) getWindow().getDecorView())
+                .setDuration(6000)
+                .setShapeCount(66)
+                .setRadiusRange(10, 20)
+                .setRotationSpeedRange(-3f, 3f)
+                .setAutoHide(true)
+                .setColors(colors)
+                .setSpeedRange(0.2f, 0.6f)
+                .setRotationRange(90, 150)
+                .setInterpolator(new AccelerateDecelerateInterpolator())
+                .build();
+        PanterDialog panterDialog = new PanterDialog(this)
+                .setHeaderBackground(R.drawable.pattern_bg_yellow)
+                .setTitle("Happy Birthday！")
+                .setPositive("I GOT IT")
+                .setNegative("Cancel")
+                .setMessage("Happy Birthday,\nShinonon. O(∩_∩)O~\n" +
+                        "唐钱进同学生日快乐")
+                .isCancelable(false);
+        panterDialog.setOnDismissListener(dialog -> {
+            mBling.show(BlingType.TRIANGLE);
+            dialog.dismiss();
+        });
+        panterDialog.show();
+        TextInputEditText edUserName = findViewById(R.id.ed_username);
+        TextInputEditText edPassword = findViewById(R.id.ed_pwd);
+        Button loginButton = findViewById(R.id.btn_login);
+        loginButton.setOnClickListener(v -> Log.e(TAG,"账号为:"+edUserName+" 密码为:"+edPassword));
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mBling.release();
+    }
+
+}
